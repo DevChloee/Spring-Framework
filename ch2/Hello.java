1188bdc0e9b61236942f754c35e02d355135fbd0
DevChloee <dawon9031@gmail.com>
2022-07-19 23:50:08 -0400

    testing

--- ch2/Hello.java
+++ ch2/Hello.java
@@ -9,6 +9,8 @@ public class Hello {
 	int iv = 10; //instance variable
 	static int cv = 20; // static variable
 	
+	// testing 
+	
 	// 2.Connect "hello" URL with main() METHOD
 	// RequestMapping can call method regardless of modifier
 	// Even tho Its modifier is private, RequestMapping can approach
